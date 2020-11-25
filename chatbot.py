c0b6a430c8a4a6ef45242530878fa9bf91e7e7cc
alvarosevilla2 <70824325+alvarosevilla2@users.noreply.github.com>
2020-11-25 15:50:47 -0500

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,52 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+from chatterbot.comparisons import LevenshteinDistance
+from chatterbot.conversation import Statement
+
+chatbot=ChatBot('chatpet',
+
+    input_adapter='chatterbot.input.TerminalAdapter', 
+    output_adapter='chatterbot.output.TerminalAdapter',
+
+    trainer='chatterbot.trainers.ListTrainer',
+
+    logic_adapters = [
+        {
+            "import_path": "chatterbot.logic.BestMatch",
+            "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+        }
+    ],
+
+    preprocessors=[
+    'chatterbot.preprocessors.clean_whitespace'
+    ],
+)
+
+traine=ListTrainer(chatbot)
+trainer=ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.spanish")
+trainer.train("./chattrain.yml")
+trainer.train("./psico.yml")
+trainer.train("./conversation.yml")
+
+levenshtein_distance = LevenshteinDistance()
+
+error=Statement('Te has equivocado')#convertimos una frase en un tipo statement
+request=""
+entradaDelUsuarioAnterior=""
+
+while True:
+    if request=='salir':
+        break
+
+    request=input("Yo: ")
+    response=chatbot.get_response(request)
+    if levenshtein_distance.compare(Statement(request),error)>0.51:
+        print('Bot: ¿Qué debería haber dicho?')
+        entradaDelUsuarioCorreccion = input("Yo: ")
+        traine.train([entradaDelUsuarioAnterior,entradaDelUsuarioCorreccion]) 
+        print("Bot: He aprendiendo que cuando digas {} debo responder {}".format(entradaDelUsuarioAnterior,entradaDelUsuarioCorreccion))
+
+    entradaDelUsuarioAnterior=request
+    print('Bot: ', response)
